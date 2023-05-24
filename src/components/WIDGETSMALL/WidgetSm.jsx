36cd60b7a6c25bb6096d8813192d179265a1a0af
SkidGod4444 <dev1974sai@gmail.com>
2023-05-24 19:42:28 +0530

    HomePage

---
+++ src/components/WIDGETSMALL/WidgetSm.jsx
@@ -0,0 +1,89 @@
+import "./widgetSm.css"
+import {VisibilityTwoTone} from '@mui/icons-material';
+
+export default function WidgetSm() {
+  return (
+    <div className="widgetSm">
+        <span className="widgetSmTitle">New Join Members</span>
+        <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Anna Keller</span>
+                    <span className="widgetSmUserTitle">Software Engineer</span>
+            </div>
+            <button className="widgetSmButton">
+                <VisibilityTwoTone className="widgetSmIcon" />
+                Display
+            </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Anna Keller</span>
+                    <span className="widgetSmUserTitle">Software Engineer</span>
+            </div>
+            <button className="widgetSmButton">
+                <VisibilityTwoTone className="widgetSmIcon" />
+                Display
+            </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Anna Keller</span>
+                    <span className="widgetSmUserTitle">Software Engineer</span>
+            </div>
+            <button className="widgetSmButton">
+                <VisibilityTwoTone className="widgetSmIcon" />
+                Display
+            </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Anna Keller</span>
+                    <span className="widgetSmUserTitle">Software Engineer</span>
+            </div>
+            <button className="widgetSmButton">
+                <VisibilityTwoTone className="widgetSmIcon" />
+                Display
+            </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Anna Keller</span>
+                    <span className="widgetSmUserTitle">Software Engineer</span>
+            </div>
+            <button className="widgetSmButton">
+                <VisibilityTwoTone className="widgetSmIcon" />
+                Display
+            </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Anna Keller</span>
+                    <span className="widgetSmUserTitle">Software Engineer</span>
+            </div>
+            <button className="widgetSmButton">
+                <VisibilityTwoTone className="widgetSmIcon" />
+                Display
+            </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Anna Keller</span>
+                    <span className="widgetSmUserTitle">Software Engineer</span>
+            </div>
+            <button className="widgetSmButton">
+                <VisibilityTwoTone className="widgetSmIcon" />
+                Display
+            </button>
+            </li>
+            </ul>
+    </div>
+  )
+}
--- src/pages/HOME/Home.jsx
+++ src/pages/HOME/Home.jsx
@@ -2,13 +2,18 @@ import Charts from "../../components/CHARTS/Charts"
 import Featuredinfo from "../../components/FEATUREDINFO/Featuredinfo"
 import "./home.css"
 import {userData} from "../../dummyData"
+import WidgetLg from "../../components/WIDGETLARGE/WidgetLg"
+import WidgetSm from "../../components/WIDGETSMALL/WidgetSm"
 
 export default function Home() {
   return (
     <div className="home">
     <Featuredinfo />
     <Charts data={userData} title="User Analytics" grid dataKey="Active Users" />
-    <div className="homeWidgets"></div>
+    <div className="homeWidgets">
+    <WidgetSm />
+    <WidgetLg />
+    </div>
     </div>
   )
 }
